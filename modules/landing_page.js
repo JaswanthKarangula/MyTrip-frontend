6eb0538ab9a07634c68fa9c7d206734b7d4178a4
Jaswanth Karangula <jaswanth@Jaswanths-MacBook-Pro.local>
2022-06-04 09:37:40 +0530

    Dynamically adding cities using dom

--- modules/landing_page.js
+++ modules/landing_page.js
@@ -10,11 +10,13 @@ async function init() {
 async function fetchCities() {
    try{
 
-    //    const response = await fetch(config.backendEndpoint+"/cities");
-    //    const cities = await response.json();
-    //    return cities;
+       const response = await fetch(config.backendEndpoint+"/cities");
+       const cities = await response.json();
+       return cities;
 
-    return await (await fetch(config.backendEndpoint+"/cities")).json();
+    // const cities_ = (await (await fetch(config.backendEndpoint+"/cities")).json()).cities;
+    // console.log(cities_);
+    // return cities_;
    }
    catch{
        return null;
@@ -23,7 +25,49 @@ async function fetchCities() {
 
 
 function addCityToDOM(id, city, description, image) {
-    
+    let dataDiv = document.createElement('div');
+    dataDiv.className = "col-12 col-sm-6 col-lg-3 mb-4";
+
+
+    let tileDiv = document.createElement('div');
+    tileDiv.className = "tile";
+
+    let imageElement = document.createElement('img');
+    imageElement.src = image;
+    imageElement.alt = city;
+
+    let imageText = document.createElement('div');
+    imageText.className = "tile-text text-center";
+
+    let imageHeading = document.createElement('h5');
+    imageHeading.textContent = city;
+
+    let imageDescription = document.createElement('p');
+    imageDescription.textContent = description;
+
+    imageText.appendChild(imageHeading);
+    imageText.appendChild(imageDescription);
+
+    tileDiv.appendChild(imageElement);
+    tileDiv.appendChild(imageText);
+
+
+
+
+
+    let link = document.createElement('a');
+    link.href = `pages/adventures/?city=${id}" id = ${id}`;
+
+    link.appendChild(tileDiv);
+    dataDiv.appendChild(link);
+
+
+    let grid = document.getElementById("data");
+    grid.appendChild(dataDiv);
+
+
+
+
 
 }
 
